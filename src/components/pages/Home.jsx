27812f06ae5bd6d11c949c3b2e69dc927c427160
Kevin Tan <kevin2012tan@gmail.com>
2022-06-21 09:52:58 -0700

    added env variable with API url and personal access token

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -4,6 +4,8 @@ function Home() {
   return (
     <div>
       <h1 className='text-6xl text-primary-content'>Welcome</h1>
+      {/* {process.env.REACT_APP_GITHUB_TOKEN} */}
+      {/* CAN ACCESS ENV VARIABLES (GLOBAL) */}
     </div>
   );
 }
