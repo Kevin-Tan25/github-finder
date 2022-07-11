a5856d3dd9f4a577d56ac31e2b2e8a09e5d57d39
Kevin Tan <kevin2012tan@gmail.com>
2022-07-11 12:13:54 -0700

    added UserItem component, which displays the login of each user. added proptypes for the component

---
+++ src/components/users/UserItem.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function UserItem({ user }) {
+  return <div>{user.login}</div>;
+}
+
+UserItem.propTypes = {
+  user: PropTypes.object.isRequired,
+};
+
+export default UserItem;
