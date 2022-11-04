f42afa6fdda43b7f1bcce8d49b09dc6a2267f5bf
Kevin Tan <kevin2012tan@gmail.com>
2022-11-04 10:50:26 -0700

    useReducer hook

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -1,12 +1,12 @@
 import { createContext, useReducer } from 'react';
-import githubReducer from '../GithubReducer';
+import githubReducer from './GithubReducer';
 
 const GithubContext = createContext();
 
 const GITHUB_URL = process.env.REACT_APP_GITHUB_URL;
 const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
 
-// Children is waht ever we are surrounding provider with
+// Children is whatever we are surrounding provider with
 // This is what GIVES you the data
 export const GithubProvider = ({ children }) => {
   const initialState = {
